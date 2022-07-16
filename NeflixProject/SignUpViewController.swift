bbd489c8c0254eb24857f038ac24b1dae81fba7f
skyler <ho971122@gmail.com>
2022-07-17 01:38:15 +0900

    잭플릭스 프로젝트 화면 연결 및 경고창 기능 추가 및 각 코드 설명 주석 추가

--- NeflixProject/SignUpViewController.swift
+++ NeflixProject/SignUpViewController.swift
@@ -20,20 +20,24 @@ class SignUpViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //텍스트필드들마다 속성 설정
         designField(emailField, placeholder: "이메일 주소 또는 전화번호", keyboard: .emailAddress, isSercureTextEntry: false)
         designField(pwField, placeholder: "비밀번호", keyboard: .emailAddress, isSercureTextEntry: true)
         designField(nameField, placeholder: "닉네임", keyboard: .namePhonePad, isSercureTextEntry: false)
         designField(locationField, placeholder: "위치", keyboard: .namePhonePad, isSercureTextEntry: false)
         designField(codeField, placeholder: "추천 코드 입력", keyboard: .numberPad, isSercureTextEntry: false)
         
+        //회원가입 버튼 속성 설정
         designButton(signUpButton, textColor: .black, backgroundColor: .white, normalText: "회원가입", highlightText: "축하드립니다!", fontSize: 14)
         
+        //스위치 버튼 속성 설정
         addSwitch.setOn(true, animated: true)
         addSwitch.onTintColor = .red
         addSwitch.thumbTintColor = .black
         
     }
     
+    //텍스트필드 속성 설정 함수
     func designField(_ fieldName: UITextField, placeholder: String, keyboard: UIKeyboardType, isSercureTextEntry: Bool){
         fieldName.borderStyle = .roundedRect
         fieldName.backgroundColor = .darkGray
@@ -43,6 +47,7 @@ class SignUpViewController: UIViewController {
         fieldName.isSecureTextEntry = isSercureTextEntry
     }
     
+    //버튼 디자인 설정 함수
     func designButton(_ buttonName: UIButton, textColor: UIColor, backgroundColor: UIColor, normalText: String, highlightText: String, fontSize: CGFloat){
         buttonName.layer.cornerRadius = 5
         buttonName.tintColor = textColor
@@ -52,16 +57,53 @@ class SignUpViewController: UIViewController {
         buttonName.titleLabel?.font = .systemFont(ofSize: fontSize)
     }
     
+    //경고창 생성 함수
+    func showAlertController() {
+        
+        //1. 경고창 생성
+        let alert = UIAlertController(title: "오류", message: "이메일 또는 비밀번호를 입력하세요.", preferredStyle: .alert)
+        
+        //2. 경고창 안에 버튼 생성
+        let ok = UIAlertAction(title: "확인", style: .cancel, handler: nil)
+        
+        //3. 1에서 생성한 바탕에 2에서 생성한 버튼을 추가
+        alert.addAction(ok)
+        
+        //4. 생성한 경고창을 보여줌
+        present(alert, animated: true, completion: nil)
+        
+    }
     
+    
+    //올라온 가상 키보드 바탕화면 눌러서 내려가게 만들어주는 기능
     @IBAction func tapGestureClicked(_ sender: UITapGestureRecognizer) {
         
         view.endEditing(true)
         
     }
     
+    //회원가입 버튼을 누르면 가상 키보드 내려가게 만들어주는 기능
+    //회원가입 버튼을 누르면 경고창 생성
     @IBAction func signUpButtonClicked(_ sender: UIButton) {
         
         view.endEditing(true)
+        
+        showAlertController()
+        
+        
+        
+    }
+    
+    //추가 정보 입력 스위치를 on하면 위치와 추천코드입력창이 뜨게 해주는 기능
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        
+        if sender.isOn {
+            locationField.isHidden = false
+            codeField.isHidden = false
+        } else{
+            locationField.isHidden = true
+            codeField.isHidden = true
+        }
         
     }
     
--- NeflixProject/ViewController.swift
+++ NeflixProject/ViewController.swift
@@ -17,7 +17,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //포스터 이미지 지정
         posterView.image = UIImage(named: "poster1")
+        
+        //아래 미리보기 이미지들을 동그랗게 만들어줌
         winterButton.layer.cornerRadius = 50
         winterButton.layer.borderColor = UIColor.cyan.cgColor
         winterButton.layer.borderWidth = 10
@@ -29,6 +32,8 @@ class ViewController: UIViewController {
         avatarButton.layer.borderWidth = 10
         
     }
+    
+    //재생버튼 클릭하면 포스터가 랜덤으로 변경되는 버튼
     @IBAction func playClicked(_ sender: UIButton) {
         
         posterView.image = UIImage(named: "poster\(Int.random(in: 1...4))")
